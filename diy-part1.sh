433f1cb150af7f109af5c81271676bda7623fe2e
Michaol <michaol@gmail.com>
2020-11-14 10:40:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git jingdong https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+# Add OpenClash
+git clone -b master https://github.com/vernesong/OpenClash package/openclash
