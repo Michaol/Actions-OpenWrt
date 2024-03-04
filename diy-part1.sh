f019606bc494eb37568f4591c578ebf80dc79652
Michaol <michaol@gmail.com>
2024-03-04 12:01:27 +0800

    Squashed commit of the following:
    commit decaa5c260535c302a0d59d53b598bb409fd54dd
    Author: P3TERX <25927179+P3TERX@users.noreply.github.com>
    Date:   Sat Mar 2 16:39:16 2024 +0800
    
        🕊️

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git jingdong https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
-#Add Passwall seed
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-
-# Add OpenClash
-# git clone -b master https://github.com/vernesong/OpenClash.git package/openclash
-# sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
-
-# Add TTNODE
-# git clone https://github.com/jerrykuku/luci-app-ttnode.git package/lean/ttnode
-
-# Add Argon Theme
-# rm -rf feeds/luci/themes/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
